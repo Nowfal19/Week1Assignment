fd43e9c2fa191c2c522a179f4109f8fa6c18f062
Nowfal Ahamed <101164363+Nowfal19@users.noreply.github.com>
2022-10-18 17:08:58 +0530

    2

---
+++ week3/day2/day2/Automation.java
@@ -0,0 +1,12 @@
+package week3.day2;
+
+public class Automation {
+public static void main(String[] args) {
+	MultipleLanguage obj = new MultipleLanguage();
+	obj.python();
+	obj.java();
+	obj.javaScript();
+	obj.ruby();	
+	
+}
+}
---
+++ week3/day2/day2/MultipleLanguage.java
@@ -0,0 +1,28 @@
+package week3.day2;
+
+public class MultipleLanguage implements Language {
+
+	public void python() {
+		System.out.println("Python is commonly used for developing websites and software, task automation ");
+	}
+
+	public void java() {
+		System.out.println("Java is used in many IT fields");
+	}
+
+	public void javaScript() {
+		System.out.println("Javascript used for Web sites designing");
+
+	}
+
+	public void ruby() {
+		System.out.println("Ruby is a leading Framework");
+
+	}
+//public static void main(String[] args) {
+//	MultipleLanguage obj = new MultipleLanguage();
+//	obj.java();
+//	obj.javaScript();
+//	obj.ruby();
+//}
+}
